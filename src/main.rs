41b7c95a942503a16b2378c1e028df5edfb02cef
Akshat Shukla <shuklaakshatascom@gmail.com>
2023-11-25 18:42:40 +0530

    Added ini config support

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use actix_web::{
 };
 use actix_files::Files;
 
+use configparser::ini::Ini;
+
 use r2d2::Pool;
 use r2d2_sqlite::{self, SqliteConnectionManager};
 
@@ -17,17 +19,37 @@ use routes::{
     get_songs_by_artist, get_stream_by_id,
 };
 
-use std::fs::create_dir_all;
+use std::{fs::create_dir_all, collections::HashMap};
 use std::path::PathBuf;
 
-async fn setup_db(pool: &Pool<SqliteConnectionManager>) {
+async fn setup_db(pool: &Pool<SqliteConnectionManager>, config: &HashMap<String, Option<String>>) {
     db::create_tables(pool).await;
 
-    // @todo! Read the path from config;
-    let path: PathBuf = PathBuf::from("D:\\Music\\Test");
+    let def_path: Option<String> = Some(String::from("D:\\Music\\Test"));
+    let config_path: String = config.get("rootdir").unwrap_or(&def_path).clone().unwrap();
+
+    let path: PathBuf = PathBuf::from(config_path);
     let info = utils::scan_dir(path);
 
     db::add_songs(pool, info).await.unwrap();
+}
+
+fn load_config() -> HashMap<String, Option<String>> {
+    let config_path: PathBuf = PathBuf::from("./config.ini");
+
+    if let Ok(_) = std::fs::metadata(&config_path) {
+        let mut ini: Ini = Ini::new();
+        let map = ini.load(config_path).unwrap();
+        let config = map.get("config").unwrap();
+
+        if config.is_empty() {
+            return HashMap::new();
+        } else {
+            return config.clone();
+        }
+    }
+
+    HashMap::new()
 }
 
 #[actix_web::main]
@@ -37,7 +59,8 @@ async fn main() -> std::io::Result<()> {
     let manager = SqliteConnectionManager::file("data/db.sqlite");
     let pool = Pool::new(manager).unwrap();
 
-    setup_db(&pool).await;
+    let config = load_config();
+    setup_db(&pool, &config).await;
 
     println!("Started server at PORT 8000!");
     HttpServer::new(move || {
